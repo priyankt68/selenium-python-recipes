7e22ccc08f70d9263b348d27c2f29bdab8ee60ac
priyankt68 <priyank.trivedi@unacademy.com>
2023-03-04 16:28:25 +0530

    adding element screenshot capability

--- selenium_screenshot.py
+++ selenium_screenshot.py
@@ -1,11 +1,26 @@
+import selenium
+
+
 def take_screenshot():
     from selenium import webdriver
+    from selenium.webdriver.common.by import By
+
     import time
 
     driver = webdriver.Chrome()
 
-    driver.get("https://lambdatest.com")
+    driver.get("https://www.lambdatest.com/blog/best-test-automation-frameworks-2021")
     driver.save_screenshot("screenshot_lambdatest.png")
+
+    try:
+        element = driver.find_element(By.ID, 'content')
+    except selenium.common.exceptions.NoSuchElementException as e:
+        raise Exception(e)
+    else:
+        try:
+            element.screenshot("blog_content.png")
+        except IOError as ioerror:
+            raise ioerror
 
     time.sleep(4)
     driver.quit()
