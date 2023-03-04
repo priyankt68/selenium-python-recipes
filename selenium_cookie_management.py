65ff7ee5a5b157ddc32754e7657f6a66167c3d37
priyankt68 <priyank.trivedi@unacademy.com>
2023-03-04 17:17:43 +0530

    adding cookie support

--- selenium_cookie_management.py
+++ selenium_cookie_management.py
@@ -1,30 +1,19 @@
-import selenium
-
-
-def take_screenshot():
+def cookie_manager():
     from selenium import webdriver
-    from selenium.webdriver.common.by import By
-
     import time
 
     driver = webdriver.Chrome()
 
-    driver.get("https://www.lambdatest.com/blog/best-test-automation-frameworks-2021")
-    driver.save_screenshot("screenshot_lambdatest.png")
+    driver.get("https://www.lambdatest.com/")
+    driver.add_cookie({"name": "user", "domain": "lambdatest.com", "value": "automation_user"})
+    all_cookies = driver.get_cookies()
+    for cookie in all_cookies:
+        print(cookie)
 
-    try:
-        element = driver.find_element(By.ID, 'content')
-    except selenium.common.exceptions.NoSuchElementException as e:
-        raise Exception(e)
-    else:
-        try:
-            element.screenshot("blog_content.png")
-        except IOError as ioerror:
-            raise ioerror
 
     time.sleep(4)
     driver.quit()
 
 
 if __name__ == '__main__':
-    take_screenshot()
+    cookie_manager()
